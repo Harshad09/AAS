57ba3fb3091e01f10307b5acd22f8bb77a66a576
Harshad <hdabhade7@gmail.com>
2021-03-19 16:51:14 +0530

    Adding files to AAS

---
+++ app/src/main/java/com/example/anantagritech/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.anantagritech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        getActionBar().hide();
+
+        mWebView  = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://ananthagriservices.in/");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+}
